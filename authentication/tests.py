e820bbb694380e34d38a674ca00efa481c969a56
Lom-Ali <lomaliyakubov@gmail.com>
2019-01-10 19:10:42 +0300

    Fix username tests and add tests for email

--- authentication/tests.py
+++ authentication/tests.py
@@ -94,6 +94,44 @@ class UserTest(APITestCase):
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(len(response.data['username']), 1)
 
+    def test_create_user_with_preexisting_email(self):
+        data = {
+            'username': 'johndoe',
+            'email': 'test@example.com',
+            'password': 'johndoe222'
+        }
+
+        response = self.client.post(self.signup_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(len(response.data['email']), 1)
+
+    def test_create_user_with_invalid_email(self):
+        data = {
+            'username': 'johndoe',
+            'email': 'johndoe',
+            'password': 'johndoe222'
+        }
+
+        response = self.client.post(self.signup_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(len(response.data['email']), 1)
+
+    def test_create_user_with_no_email(self):
+        data = {
+            'username': 'johndoe',
+            'email': '',
+            'password': 'johndoe222'
+        }
+
+        response = self.client.post(self.signup_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(len(response.data['email']), 1)
+
+
+
 
 
 
